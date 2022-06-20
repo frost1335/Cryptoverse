a27c4fa3c0928d9aacc13e813dfeb91d124c57db
raximvodil <raximvodilrozbek@gmail.com>
2022-06-20 13:31:35 +0300

    20.06.2022 13:32
    Video stopper at 1:02

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
@@ -7,8 +7,31 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const Cryptocurrencies = () => {
   const { data: cryptoList, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
 
-  return <div>Cryptocurrencies</div>;
+  console.log(cryptos);
+
+  return (
+    <>
+      <Row gutters={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Cryptocurrencies;
