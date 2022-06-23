606dad67cb8162f8a77af94609053ead3014ead9
raximvodil <raximvodilrozbek@gmail.com>
2022-06-23 11:21:49 +0300

    23.06.2022 11:21
    Project finished , but Exchanges component doesnt work, cause of exchanges api only for premium

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,49 +1,32 @@
 import React from "react";
-import { Line, Bar, Doughnut } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
 
 const { Title } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-  //   const coinPrice = [];
-  //   const coinTimestamp = [];
+  const coinPrice = [];
+  const coinTimestamp = [];
 
-  //   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-  //     coinPrice.push(coinHistory?.data?.history[i].price);
-  //   }
+  for (let i = 0; i < coinHistory?.data?.history?.length; i++) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+    coinTimestamp.push(
+      new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
+    );
+  }
 
-  //   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-  //     coinTimestamp.push(
-  //       new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
-  //     );
-  //   }
-
-  //   console.log(coinPrice, coinTimestamp);
-
-  //   const data = {
-  //     labels: coinTimestamp,
-  //     datasets: [
-  //       {
-  //         label: `Price in USD`,
-  //         data: coinPrice,
-  //         fill: false,
-  //         backgroundColor: "#0071bd",
-  //         borderColor: "#0071bd",
-  //       },
-  //     ],
-  //   };
-
-  //   const options = {
-  //     scales: {
-  //       yAxis: [
-  //         {
-  //           ticks: {
-  //             beginAtZero: true,
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   };
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: `Price in USD`,
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
 
   return (
     <>
@@ -60,11 +43,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
           </Title>
         </Col>
       </Row>
-      <Doughnut
-        data={{
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        }}
-      />
+      <Line data={data} />
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button, Menu, Typography, Avatar } from "antd";
+import React, { useState, useEffect } from "react";
+import { Menu, Typography, Avatar, Button } from "antd";
 import { Link } from "react-router-dom";
 import {
   HomeOutlined,
@@ -12,6 +12,27 @@ import {
 import icon from "../images/cryptocurrency.png";
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -19,21 +40,29 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />} key={1}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />} key={2}>
-          <Link to="/cryptocurrencies">Cryptocurrencies </Link>
-        </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />} key={3}>
-          <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />} key={4}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
+      {activeMenu && (
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />} key={1}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />} key={2}>
+            <Link to="/cryptocurrencies">Cryptocurrencies </Link>
+          </Menu.Item>
+          {/* <Menu.Item icon={<MoneyCollectOutlined />} key={3}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item> */}
+          <Menu.Item icon={<BulbOutlined />} key={4}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   );
 };
